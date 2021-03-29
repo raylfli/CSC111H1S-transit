0475ea6f62cae0de2ab0bc91e6ab1f7c07891327
charleswong739 <45798963+charleswong739@users.noreply.github.com>
2021-03-28 23:19:13 -0400

    A* stubs and distance formula

---
+++ pathfinding.py
@@ -0,0 +1,17 @@
+"""A* shenanigans"""
+
+
+def find_route(start_loc, end_loc, time_bloc):
+    """Given a start location, end location, and time block, compute the quickest transit route."""
+
+
+def a_star(curr, goal) -> None:
+    """A* algorithm for graph pathfinding."""
+
+
+def h(curr, goal) -> float:
+    """A* heuristic function.
+
+    In this particular case, calculate the great-circle distance between the curr and goal nodes
+    using the haversine formula.
+    """

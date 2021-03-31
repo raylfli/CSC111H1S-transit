8894d01c77f698fe79eb70ed02ec496c33913615
charleswong739 <45798963+charleswong739@users.noreply.github.com>
2021-03-30 22:45:45 -0400

    Added all stop times to data base and renamed

--- parse_data.py
+++ parse_data.py
@@ -2,10 +2,11 @@
 
 Example to run after creating db:
 
-con = sqlite3.connect('stoptimes1.db')
+con = sqlite3.connect('stoptimes.db')
 cur = con.cursor()
-for row in cur.execute("SELECT * FROM stoptimes1 WHERE stop_id='1277'"):
+for row in cur.execute("SELECT * FROM stoptimes WHERE stop_id='1277'"):
     print(row)
+con.close()
 """
 
 import sqlite3
@@ -15,9 +16,9 @@ def create_db() -> None:
     """Create database for stoptimes1. One time use only. Hardcoded.
     TODO: make not hardcoded
     """
-    con = sqlite3.connect('stoptimes1.db')
+    con = sqlite3.connect('stoptimes.db')
     cur = con.cursor()
-    cur.execute('''CREATE TABLE stoptimes1 
+    cur.execute('''CREATE TABLE stoptimes 
                 (trip_id, arrival_time, departure_time, stop_id, stop_sequence, stop_headsign, 
                 pickup_type, drop_off_type, shape_dist_traveled)''')
     con.commit()
@@ -26,20 +27,34 @@ def create_db() -> None:
 
 def populate_db() -> None:
     """Populate database for stoptimes 1. One time use only. Hardcoded.
-    TODO: make not hardcoded
+    TODO: make not hardcoded?
     """
+    con = sqlite3.connect('stoptimes.db')
+    cur = con.cursor()
+
     with open('data/stop_times_1.txt', 'r') as f:
         header = f.readline()
 
-        con = sqlite3.connect('stoptimes1.db')
-        cur = con.cursor()
+        for line in f:
+            entry = tuple(line.strip('\n').split(','))
+            cur.execute("INSERT INTO stoptimes VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", entry)
+
+    with open('data/stop_times_2.txt', 'r') as f:
+        header = f.readline()
 
         for line in f:
             entry = tuple(line.strip('\n').split(','))
-            cur.execute("INSERT INTO stoptimes1 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", entry)
+            cur.execute("INSERT INTO stoptimes VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", entry)
 
-        con.commit()
-        con.close()
+    with open('data/stop_times_3.txt', 'r') as f:
+        header = f.readline()
+
+        for line in f:
+            entry = tuple(line.strip('\n').split(','))
+            cur.execute("INSERT INTO stoptimes VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", entry)
+
+    con.commit()
+    con.close()
 
 
 # DOESN'T WORK TOO LAZY TO FIGURE OUT RN
@@ -48,11 +63,12 @@ def populate_db() -> None:
 #     con = sqlite3.connect(db_file)
 #     cur = con.cursor()
 #
-#     cur.execute("SELECT * FROM stoptimes1 WHERE ?=?", (column, "'" + value + "'"))
+#     cur.execute("SELECT * FROM stoptimes WHERE ?=?", (column, "'" + value + "'"))
 #
 #     return cur.fetchall()
 
 
+# This might not work either idk y i thought i would need it i've never tried using it.
 def nuke(db_file: str, db: str) -> None:
     """Deletes everything from a database. USE WITH EXTREME CAUTION. FOR DEV ONLY."""
     con = sqlite3.connect(db_file)
@@ -65,17 +81,3 @@ def nuke(db_file: str, db: str) -> None:
     if inp == 'y':
         cur.execute(f'DELETE FROM ?', quotify(db))
         con.commit()
-
-
-def quotify(s):
-    """If s is a list of strings, return a tuple of the elements of s with single quotes around them.
-    If s is a string, return s with quotes around it.
-    """
-
-    if isinstance(s, list):
-        newlst = []
-        for e in s:
-            newlst.append("'" + e + "'")
-        return tuple(newlst)
-    elif isinstance(s, str):
-        return "'" + s + "'"

e0d016f3ead9e89cd8f5fc0f5a9fe6669dbae619
annaa-cho <ac.anna.cho@gmail.com>
2021-04-02 10:28:51 -0400

    added (unfinished) graph

---
+++ main.py
@@ -0,0 +1,54 @@
+"""
+CSC111 Final Project: TTC Transit Graph
+
+This module provides the implementation for the main visualizations and computations of
+this project.
+
+Running the main block of this module will:
+# TODO replace the following!
+#     1. Retrieve the necessary HURDAT2 data.
+#     2. Convert the raw data (.txt) into a usable .csv file.
+#     3. Parse the usable .csv into a list of Storm dataclasses.
+#     4. Create all visualizations.
+#     5. Generate HTML file that contains all the visualizations.
+
+This file is Copyright (c) 2021 Anna Cho, Charles Wong, Grace Tian, Raymond Li
+"""
+
+import data_interface
+from graph import Graph
+
+
+def load_graph() -> Graph():
+    """Return a transit system graph using the processed data from data_interface.py.
+
+    The transit system graph stores one vertex for each stop in the dataset.
+    Each vertex stores as its item either a Stop ID, and the "location" _Vertex attribute stores
+    the latitude and longitude of each stop.
+
+    Edges represent a one-way connection between two existing stops.
+    """
+    g = Graph()
+    vertices = data_interface.get_stops()
+
+    for vertex in vertices:
+        g.add_vertex(vertex[0], vertex[1])
+
+    return g
+
+
+if __name__ == '__main__':
+
+    load_graph()
+    # todo complete this section
+
+    import python_ta
+    # todo modify the PyTA check
+
+    python_ta.check_all(config={
+        'max-line-length': 1000,
+        'disable': ['E1136'],
+        'extra-imports': ['csv', 'networkx'],
+        'allowed-io': ['load_review_graph'],
+        'max-nested-blocks': 4
+    })

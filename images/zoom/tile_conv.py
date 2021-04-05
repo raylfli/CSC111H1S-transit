c6eaa8f3e4bb84f96c92461baf27ca4dd49a9d47
Charles <charleswonghm@gmail.com>
2021-04-04 23:12:00 -0400

    Zoomed in map + companions

---
+++ images/zoom/tile_conv.py
@@ -0,0 +1,24 @@
+"""Functions to convert tile numbers to coordinates and vice versa.
+
+See Info.txt for information on the generated map image.
+
+Source: https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#X_and_Y
+See page for more detailed information.
+"""
+import math
+
+
+def num2deg(xtile, ytile, zoom):
+    n = 2.0 ** zoom
+    lon_deg = xtile / n * 360.0 - 180.0
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+    lat_deg = math.degrees(lat_rad)
+    return (lat_deg, lon_deg)
+
+
+def deg2num(lat_deg, lon_deg, zoom):
+    lat_rad = math.radians(lat_deg)
+    n = 2.0 ** zoom
+    xtile = int((lon_deg + 180.0) / 360.0 * n)
+    ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
+    return (xtile, ytile)

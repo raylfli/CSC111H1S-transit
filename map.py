db5e70b7dd5170517ecad3ff9f1f9ac89cd778d3
Grace <gtian239@yahoo.com>
2021-04-17 02:48:59 -0400

    Add all docstrings

--- map.py
+++ map.py
@@ -27,7 +27,7 @@ DAY_TO_INT = {'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4,
 
 
 def initialize_screen(allowed: list, width: int, height: int) -> pygame.Surface:
-    """..."""
+    """Initialize a pygame screen."""
     pygame.display.init()
     pygame.font.init()
     screen = pygame.display.set_mode((width, height))
@@ -39,7 +39,7 @@ def initialize_screen(allowed: list, width: int, height: int) -> pygame.Surface:
 
 
 def load_zoom_image(images: dict[int, Image], zoom: int = 0) -> pygame.Surface:
-    """..."""
+    """Return image corresponding to the given zoom level as a pygame.Surface."""
     # Choose image to load based on zoom
     image = images[zoom]
     return pygame.image.load(image.file)
@@ -47,17 +47,13 @@ def load_zoom_image(images: dict[int, Image], zoom: int = 0) -> pygame.Surface:
 
 def draw_map(screen: pygame.Surface, tile: Union[pygame.Surface, pygame.SurfaceType],
              x: int, y: int) -> None:
-    """..."""
+    """Draw the map on the given screen."""
     # Draw image
     screen.blit(tile, (x, y))
 
 
 def draw_zoom_in(screen: pygame.Surface, x: int, y: int, width: int, height: int) -> None:
-    """ ...
-
-    Precondition:
-        - button[0].height > button[1].height
-
+    """Draw function for zoom in button.
     """
     pygame.draw.line(screen, pygame.Color('black'),  # horizontal line
                      (x + width / 10, y + height / 2), (x + width - width / 10, y + height / 2), 7)
@@ -66,14 +62,14 @@ def draw_zoom_in(screen: pygame.Surface, x: int, y: int, width: int, height: int
 
 
 def draw_zoom_out(screen: pygame.Surface, x: int, y: int, width: int, height: int) -> None:
-    """Draw function for zoom out"""
+    """Draw function for zoom out button."""
     pygame.draw.line(screen, pygame.Color('black'),  # horizontal line
                      (x + width / 10, y + height / 2), (x + width - width / 10, y + height / 2), 7)
 
 
 def draw_waypoints(screen: pygame.Surface, image: Image, waypoints: list,
                    orig_x: int, orig_y: int) -> None:
-    """dkm this isn't acc drawing waypoints"""
+    """Draw function for waypoints."""
     for i in range(min(len(waypoints), 2)):
         waypoints[i].draw(screen, image, orig_x, orig_y)
 
@@ -90,14 +86,9 @@ def draw_progress(screen: pygame.Surface, x: int, y: int, width: int, height: in
     pygame.draw.rect(screen, (180, 180, 180), (x, y, width * eval(text[:-1]) / 100, height))
 
 
-def scroll_diff(p: int, mouse_p) -> int:
-    """..."""
-    return p - mouse_p
-
-
 def continue_scroll(image: Image, width: int, height: int, x: int, y: int,
                     mouse_x: int, mouse_y: int, x_diff: int, y_diff: int) -> tuple[int, int]:
-    """..."""
+    """Return the (x, y) coordinates of the top left corner of the image after scrolling."""
     new_x, new_y = mouse_x + x_diff, mouse_y + y_diff
     if not (new_x > 0 or new_x + image.width < width) and \
             not (new_y > 0 or new_y + image.height < height):
@@ -111,8 +102,7 @@ def clamp(num, min_value: int = 0, max_value: int = 3):
     return max(min(num, max_value), min_value)
 
 
-def check_points_clicked(lat: float, lon: float,
-                         waypoints: list) -> None:
+def check_points_clicked(lat: float, lon: float, waypoints: list) -> None:
     """Mutate points to reflect if the point is the starting or ending destination"""
     if waypoints == []:
         waypoints.append(Waypoint(lat, lon, chosen_start=True))
@@ -120,17 +110,14 @@ def check_points_clicked(lat: float, lon: float,
         waypoints.append(Waypoint(lat, lon, chosen_end=True))
 
 
-def run_map(filename: str = "data/image_data/images_data.csv",
-            width: int = 1000, height: int = 600) -> None:
+def run_map(filename: str = "data/image_data/images_data.csv") -> None:
     """Run main map.
 
     Preconditions:
-        - 150 <= width <= 1000
-        - 150 <= height <= 800
         - 43.52434 <= lat <= 43.96036
         - -79.81884 <= lon <= -79.06096
     """
-    screen = initialize_screen(ALLOWED, width, height)
+    screen = initialize_screen(ALLOWED, 1000, 600)
 
     # map screen
     map_bound = Rect(200, 0, 800, 600)
@@ -354,8 +341,7 @@ def run_map(filename: str = "data/image_data/images_data.csv",
                 True, False, False):
 
             if map_bound.contains(mouse_x, mouse_y) and not clicked:
-                x_diff, y_diff = scroll_diff(x, mouse_x - map_bound.x), \
-                                 scroll_diff(y, mouse_y - map_bound.y)
+                x_diff, y_diff = x - (mouse_x - map_bound.x), y - (mouse_y - map_bound.y)
                 down = True
 
         if event.type == pygame.MOUSEBUTTONUP:
@@ -377,14 +363,13 @@ def run_map(filename: str = "data/image_data/images_data.csv",
                 mouse_x -= map_bound.x
                 mouse_y -= map_bound.y
                 scroll = True
-                new_x, new_y = continue_scroll(images[zoom], map_bound.width, map_bound.height, x,
-                                               y,
-                                               mouse_x, mouse_y, x_diff, y_diff)
+                new_x, new_y = continue_scroll(images[zoom], map_bound.width, map_bound.height,
+                                               x, y, mouse_x, mouse_y, x_diff, y_diff)
                 if new_x == x:
-                    x_diff = scroll_diff(x, mouse_x)
+                    x_diff = x - mouse_x
                 else:
                     x = new_x
                 if new_y == y:
-                    y_diff = scroll_diff(y, mouse_y)
+                    y_diff = y - mouse_y
                 else:
                     y = new_y
